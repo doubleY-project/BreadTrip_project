5dea243f7ae5f1fc24803ca32cfd24c365a681c3
yujeen02 <yujin5479@gmail.com>
2025-01-13 18:38:16 +0900

    Feat. 유진

--- videoMent.js
+++ videoMent.js
@@ -6,7 +6,7 @@ ddaySet.style.opacity = 0;
 
 let offsetX = 0;
 
-// canvas 크기 조정
+// canvas 크기 조정 및 캔버스의 글자를 투명하게 겹치게 하는 함수
 function resizeCanvas() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
@@ -18,6 +18,7 @@ function resizeCanvas() {
     ctx.font = "bold 550px Arial";
     ctx.textAlign = "center";
     ctx.textBaseline = "middle";
+
     ctx.fillText(
         "Welcome to BreadTrip",
         canvas.width / 2 + offsetX * 2.5,
@@ -25,6 +26,7 @@ function resizeCanvas() {
     );
 }
 
+// 화면의 왼쪽 끝으로 이동하면 오르쪽 끝으로 초기화되는 함수
 function animateText() {
     offsetX -= 5;
     if (offsetX <= -canvas.width) {
@@ -34,6 +36,7 @@ function animateText() {
     requestAnimationFrame(animateText);
 }
 
+// 아래로 스크롤되면 캔버스는 올라가게 되고 dday 가 나옴, 위로 스크롤 되면 반대로 되는 함수
 function handleScroll() {
     const scrollY = window.scrollY;
     const viewportHeight = window.innerHeight;
@@ -41,15 +44,12 @@ function handleScroll() {
     if (scrollY > 0) {
         canvas.style.transform = `translateY(-${viewportHeight}px)`;
         canvas.style.opacity = 0;
-
         ddaySet.style.opacity = 1;
-
-        // diffDay 강제 호출 및 초기화
         window.diffDay();
+
     } else {
         canvas.style.transform = "translateY(0)";
         canvas.style.opacity = 1;
-
         ddaySet.style.opacity = 0;
     }
 }
